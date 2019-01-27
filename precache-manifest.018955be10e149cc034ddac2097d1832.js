23bfcc27e126b88be578020679a0eeaa13cdc1e8
elBotija <mauroenpixels@gmail.com>
2019-01-27 15:14:26 -0300

    Updates

---
+++ precache-manifest.018955be10e149cc034ddac2097d1832.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "117c63c06a43dc8a85ab",
+    "url": "/turnos-redux/static/js/runtime~main.117c63c0.js"
+  },
+  {
+    "revision": "c7f59c34ac2e0c1402cd",
+    "url": "/turnos-redux/static/js/main.c7f59c34.chunk.js"
+  },
+  {
+    "revision": "d46135748bb9f6f654ff",
+    "url": "/turnos-redux/static/js/1.d4613574.chunk.js"
+  },
+  {
+    "revision": "c7f59c34ac2e0c1402cd",
+    "url": "/turnos-redux/static/css/main.d6e9d641.chunk.css"
+  },
+  {
+    "revision": "28ed5278472dcf7a3c57256e4a72ec30",
+    "url": "/turnos-redux/index.html"
+  }
+];
